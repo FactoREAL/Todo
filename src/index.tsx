bb0e3c1107c3c3d1fec1ab3d602b8669c28ed9fb
FactoREAL <factoreal@mail.ru>
2020-01-22 22:31:25 +0300

    refactor: App component reworked

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,26 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import GroupList from "./components/GroupList";
 import TaskListContainer from "./containers/TaskListContainer";
 import allReducers from "./data/reducers";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 const store = createStore(allReducers);
 
 store.subscribe(() => console.log(store.getState()));
 
-function App() {
-  return (
-    <div className="container mt-2">
-      <div className="row">
-        <GroupList/>
-        <TaskListContainer />
-      </div>
+const App: React.FC = () => (
+  <div className="container mt-2">
+    <div className="row">
+      <GroupList />
+      <TaskListContainer />
     </div>
-  );
-}
+  </div>
+);
 
 ReactDOM.render(
   <Provider store={store}>
